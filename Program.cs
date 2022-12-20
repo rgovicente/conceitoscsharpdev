c8c3a57a97f1d71af364195c8e259e4de9bd880d
Rodrigo Vicente <116674940+rgovicente@users.noreply.github.com>
2022-12-20 15:48:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace PrimeiroProjeto {
+    class Program {
+        static void Main(string[] args) {
+
+            bool completo = false;
+            char genero = 'F';
+            char letra = '\u0041';
+            byte n1 = 254;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            string nome = "Maria Green";
+            object obj1 = "Alex Brown";
+            object obj2 = 4.5f;
+            decimal n8 = decimal.MaxValue;
+            int n9 = int.MinValue;
+            int n10 = int.MaxValue;
+            sbyte n11 = sbyte.MinValue;
+
+
+
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(letra);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+            Console.WriteLine(n8);
+            Console.WriteLine(n9);
+            Console.WriteLine(n10);
+            Console.WriteLine(n11);
+
+        }
+    }
+}
